66def313492a47af1487c3b9596075caaecd4654
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-12 20:44:28 +0000

    reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,26 +7,38 @@ class FileStorage:
 
     __file_path = file_path
     __objects = {}
+
     def all(self):
         """Return dictionary of __objects."""
         return self.__objects
+
     def new(self, obj):
         """Set in __objects the obj with key <obj class name>.id."""
         my_dict = self.__objects
         id = str(uuid.uuid4())
-        my_dict["{}.{}".format(type(self)__name__, id)] = obj
+        my_dict["{}.{}".format(type(self).__name__, id)] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file."""
-        my_dict =  self.__objects
+        """Serialize __objects to the JSON file."""
+        my_dict = self.__objects
         my_obj = {}
+
         for key, value in my_dict.items():
             my_obj[key] = value.to_dict()
         my_obj_serialized = dump(my_obj)
         with open(self.__file_path, 'w') as file:
-            file.write(my_obj_serialized)
+            json.dump(my_obj_serialized, file)
 
-    def  reload(self):
+    def reload(self):
         """Deserialize the JSONfile to __objects only if __file_path exists."""
-        pass
+        try:
+            with open(FileStorage.__file_path) as file:
+                obj = json.load(file)
+            for key, value in obj.items():
+                class_name = value['__class__']
+                class_obj = globals()[class_name]
+                instance = class_obj(**value)
+                self.new(instance)
 
+        except FileNotFoundError:
+            pass
