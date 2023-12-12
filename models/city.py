5e2e89ec4dbc8df853327efe4f0d61dfa9b36bd7
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-12 22:12:51 +0000

    city

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class City."""
+from models.base_models import BaseModel
+
+
+class City(BaseModel):
+    """Class City that inherits from BaseModel."""
+
+    state_id = ""
+    name = ""
