9deca139c4e5f09fc1bb4e4dcf0895ecc36d5da2
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-12 22:29:48 +0000

    review

---
+++ models/review.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Class Review."""
+from models.base_models import  BaseModel
+
+
+class Review(BaseModel):
+    """Class Review."""
+
+    place_id = ""
+    user_id = ""
+    text = ""`
