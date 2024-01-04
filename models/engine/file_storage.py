90463d9bfa35b070baf8a750182d660afdea4c4b
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-04 00:05:19 +0000

    corected error 1

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,13 +5,13 @@ from models.base_model import BaseModel
 import models
 
 
-class FileStoarage:
+class FileStorage:
     """Defines the class FileStorage."""
 
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Return the dictionary __objects."""
         return self.__objects
 
@@ -26,13 +26,13 @@ class FileStoarage:
 
         for key in self.__objects:
             this_dict[key] = self.__objects[key].to_dict()
-        with open(__file_path, 'w') as file:
+        with open(self.__file_path, 'w') as file:
             json.dump(this_dict, file)
 
     def reload(self):
         """Deserialize the Json file to __objects."""
         try:
-            with open(__file_path, 'r', encoding=UTF8) as file:
+            with open(self.__file_path, 'r', encoding='UTF8') as file:
                 this_new_dict = json.load(file)
 
             for value in this_new_dict.values():
